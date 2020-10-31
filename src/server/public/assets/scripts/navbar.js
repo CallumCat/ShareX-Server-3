19ed5c87755faeddd250e339c5cff92b73bee1ca
Million900o <apaolini900o@gmail.com>
2020-10-31 03:31:11 +0000

    adding signup

--- src/server/public/assets/scripts/navbar.js
+++ src/server/public/assets/scripts/navbar.js
@@ -4,5 +4,6 @@ function navbar() {
     <a href="https://netdata.terano.dev">Netdata</a>
     <a href="/pages/upload">Upload</a>
     <a href="/pages/files">Files</a>
+    <a href="/pages/signup">Sign Up</a>
     <a class="right" href="https://github.com/Million900o">Github</a>`;
 }
