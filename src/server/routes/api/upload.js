82818fb25b11da75c8e4f52e7ba566c4c6684ffc
Million900o <apaolini900o@gmail.com>
2020-11-07 23:33:16 +0000

    bug 2

--- src/server/routes/api/upload.js
+++ src/server/routes/api/upload.js
@@ -56,8 +56,8 @@ router.post('/', auth, (req, res) => {
   req.files.file.mv(uploadPath, async err => {
     if (err) return res.status(500).send(err);
 
-    let lockActive = req.body.locked || false;
-    let lockPassword = req.body.password || 'none';
+    // let lockActive = req.body.locked || false;
+    // let lockPassword = req.body.password || 'none';
 
     await saveFile({
       originalName: req.files.file.name,
@@ -66,10 +66,10 @@ router.post('/', auth, (req, res) => {
       name: name,
       UploadedAt: new Date(),
       views: 0,
-      lock: {
-        active: lockActive,
-        password: lockPassword,
-      },
+      // lock: {
+      //   active: lockActive,
+      //   password: lockPassword,
+      // },
     });
 
     let linkPart = req.userData.domain === undefined || req.userData.domain === 'none' ?
