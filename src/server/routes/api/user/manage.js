c462b90005045dc92cfd2957d9143b18aaadb0cc
Million900o <apaolini900o@gmail.com>
2020-11-11 11:20:33 +0000

    TODO

--- src/server/routes/api/user/manage.js
+++ src/server/routes/api/user/manage.js
@@ -8,17 +8,7 @@ router.use(json());
 router.use(urlencoded({ extended: false }));
 
 router.post('/', browserAuth, async (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let passwordCheck = req.body.password2;
-  if (!username || !password)
-    return res.redirect('/dashboard?error=Please use a valid username and password');
-
-  let passwordHash = sha256(password);
-  let userData = await getUserFromPassword(username, passwordHash);
-  if (!userData) return res.redirect('/dashboard?error=Please use a valid username and password');
-
-  return res.redirect(req.query.r || '/dashboard');
+  // TODO
 });
 
 module.exports = router;
